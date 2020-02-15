d076e18e20e9dc58535553fa312fe3821cc1b241
joel <joelcrawford30@gmail.com>
2020-02-15 12:55:00 +0300

    modification

--- app/src/main/java/com/example/messenger/LoginActivity.kt
+++ app/src/main/java/com/example/messenger/LoginActivity.kt
@@ -1,16 +1,22 @@
 package com.example.messenger
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
     //usually overide on create method
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        //-======setting view for the activity=====
-
+        //======setting view for the activity=====
         setContentView(R.layout.activity_login)
+
+        login_text_view.setOnClickListener {
+            ///====launch the login activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
