2687a5673f6863e63f6eac5c415a0d0a9b46068b
joel <joelcrawford30@gmail.com>
2020-02-15 14:46:48 +0300

    Rounded corners for both Register and Login text fields, firebase integration

--- app/src/main/java/com/example/messenger/LoginActivity.kt
+++ app/src/main/java/com/example/messenger/LoginActivity.kt
@@ -2,7 +2,9 @@ package com.example.messenger
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -14,6 +16,25 @@ class LoginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         login_text_view.setOnClickListener {
+
+            val email = email_login.text.toString()
+            val password = password_login.text.toString()
+
+            //========firebase auth====
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    //======else===
+                    Log.d("Login", "successfully created user with the UId: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener {
+                    Log.d("Login", "Failed to Login ${it.message}")
+                }
+
+
+
+
             ///====launch the login activity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/messenger/MainActivity.kt
+++ app/src/main/java/com/example/messenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,13 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         register_button_register.setOnClickListener {
 
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_register.text.toString()
-
-            Log.d("MainActivity", "Email is" +  email)
-            Log.d("MainActivity", "Password is $password")
-
-            //========firebase auth to perform create user with email and password====
+           performRegister()
         }
 
         already_have_account_text_view.setOnClickListener {
@@ -30,5 +26,42 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private  fun performRegister() {
+
+
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        Log.d("MainActivity", "Email is" +  email)
+        Log.d("MainActivity", "Password is $password")
+
+        //===check for the fields not to be empty
+        //if (email.isEmpty() || password.isEmpty()) return@setOnClickListener using toast
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please Enter Email Address and Password", Toast.LENGTH_LONG).show()
+            return
+
+        }
+
+
+        //========firebase auth to perform create user with email and password====
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+
+                //else if successfull
+                Log.d("Main", "successfully created user with the UId: ${it.result?.user?.uid}")
+
+
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed to create user ${it.message}")
+                Toast.makeText(this, "Failed to create user ${it.message}", Toast.LENGTH_LONG).show()
+            }
+
+
     }
 }
